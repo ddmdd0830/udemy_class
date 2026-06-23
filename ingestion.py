0a7ba4c3aa1f2c28ae44537377e807bbb24122ca
Xinjun Dong <xinjundong@gmail.com>
2026-06-23 15:44:58 -0400

    add tavily_crawl

---
+++ ingestion.py
@@ -0,0 +1,69 @@
+import asyncio
+import os
+import ssl
+from typing import Any, Dict, List
+
+import certifi
+from dotenv import load_dotenv
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+# from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
+
+
+from logger import (Colors, log_error, log_header, log_info, log_success,
+                    log_warning)
+
+load_dotenv()
+
+
+# Configure SSL context to use certifi certificates
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-small",
+    show_progress_bar=False,
+    chunk_size=50,
+    retry_min_seconds=10,
+)
+
+vectorstore = PineconeVectorStore(
+    index_name="langchain-doc-index", embedding=embeddings
+)
+tavily_extract = TavilyExtract()
+tavily_map = TavilyMap(max_depth=5, max_breath=20, max_pages=1000)
+tavily_crawl = TavilyCrawl()
+
+
+
+
+
+
+async def main():
+    # Your asynchronous code here
+    log_header("DOCUMENTATION INGESTION PIPELINE")
+
+    log_info("TavilyCrawl: Starting to Crawl documentation from https://python.langchain.com/",
+             Colors.PURPLE)
+    
+    res = tavily_crawl.invoke(
+        {
+            "url": "https://python.langchain.com/",
+            "max_depth": 5,
+            "extract_depth": "advanced",
+            "instructions": "content on ai agents"           
+        }
+    )
+    
+    all_docs = [Document(page_content=result['raw_content'], metadata={"source": result['url']}) for result in res['results']]
+    log_success(
+        f"TavilyCrawl: Successfully crawled {len(all_docs)} documents from documentation site")
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main()) 
