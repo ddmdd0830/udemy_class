28154dd6ae8f2468893f9732667b4e45adf111f6
Xinjun Dong <xinjundong@gmail.com>
2026-07-06 10:58:06 -0400

    client implementation

---
+++ langchain_client.py
@@ -0,0 +1,20 @@
+from langchain_mcp_adapters.client import MultiServerMCPClient
+
+from langchain.agents import create_agent
+
+from langchain_openai import ChatOpenAI
+
+from dotenv import load_dotenv
+import asyncio
+
+load_dotenv()
+
+llm = ChatOpenAI()
+
+
+async def main():
+    print("hello langchain mcp client")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- main.py
+++ main.py
@@ -3,11 +3,35 @@ import asyncio
 from dotenv import load_dotenv
 load_dotenv()
 
+from langchain_core.messages import HumanMessage
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/xl/Documents/xd_github/udemy_class/servers/math_server.py"],
+    
+)
+
+llm = ChatOpenAI()
 
 
 async def main():
-    print("Hello from mcp-crash-course")
+    async with stdio_client(stdio_server_params) as (read,write):    
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            tools = await load_mcp_tools(session)
 
 
-if __name__ == '__main__':
+            agent = create_agent(llm,tools)
+
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
+
+if __name__ == "__main__":
     asyncio.run(main())
+
